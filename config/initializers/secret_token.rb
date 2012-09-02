7c6407608a898c1d8902e237f15753017a2be375
Carson Tang <tang.carson@gmail.com>
2012-09-02 15:15:53 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Craven::Application.config.secret_token = 'c9fc45713dbdbf31be2fca9b788d9db265a30d00215b7183320e5c61e62043bf24884046a1c186dfe3c522e1dcb1e97cd32b2caee3adfcfe45bc06d74f27f09e'
